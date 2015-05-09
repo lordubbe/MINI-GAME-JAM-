1c3b64c72f1d3352788baeca3d16c7ee0033697f
lordubbe <jacoblynggaardolsen@hotmail.com>
2015-05-09 16:41:05 +0200

    asd

--- Mini Game Jam/Assets/shoot.cs
+++ Mini Game Jam/Assets/shoot.cs
@@ -1,43 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.Audio;
 public class shoot : MonoBehaviour {
 	public Transform bulletSpawn;
 	public Transform bullet;
+	public float bulletSpeed;
 	public Transform player;	
 	private Vector3 shootDir;
-
+	private ParticleSystem shootParticles;
 	public bool shooting;
 	public float shootSpeed;
+	public float shotSpread;
+	public float bulletKnockback;
+	public AudioClip shotSound;
+	public AudioClip shellSound;
+
 	// Use this for initialization
 	void Start () {
-		
+		shootParticles = bulletSpawn.GetComponent<ParticleSystem> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		shootDir = bulletSpawn.position+Vector3.Normalize(bulletSpawn.position-player.position);
-		Debug.DrawLine (bulletSpawn.position, shootDir, Color.red);
+		shootDir = Vector3.Normalize (player.transform.position - bulletSpawn.transform.position)*-1;
+		Debug.DrawRay (bulletSpawn.position, shootDir, Color.red);
 	
 	
 		handleMouseInputs ();
-		if (shooting) {
-			print("nigger");
-		}
+
+
 	}
 
 	void handleMouseInputs(){
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {//if shooting
+			Camera.main.GetComponent<CameraShake>().enabled = true;
+
 			shooting = true;
-		} else if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			shootParticles.enableEmission = true;
+			bulletSpawn.GetChild (0).GetComponent<ParticleSystem>().enableEmission = true;
+			StartCoroutine("fireBullet", shootSpeed);
+		} else if (Input.GetKeyUp (KeyCode.Mouse0)) {//if not
+			Camera.main.GetComponent<CameraShake>().enabled = false;
 			shooting = false;
+			transform.localScale = new Vector3(1,1,1);
+			shootParticles.enableEmission = false;
+			bulletSpawn.GetChild (0).GetComponent<ParticleSystem>().enableEmission = false;
+			StopCoroutine("fireBullet");
 		}
 	}
 
 	IEnumerator fireBullet(float shootInterval){
 		while (shooting) {
-			Transform bullet = (Transform)Instantiate (bullet, bulletSpawn.position, Quaternion.identity) as Transform;
+	
+
+			player.GetComponent<Rigidbody2D>().AddForce((shootDir*bulletKnockback)*-1);
+
+		//	transform.localScale = new Vector3(Random.Range (0.8f,1.2f),1,1);
+			GetComponent<AudioSource>().pitch = Random.Range (1.5f, 2.0f);
+			GetComponent<AudioSource>().volume = Random.Range(0.3f, 0.4f);
+			GetComponent<AudioSource>().PlayOneShot(shotSound);
+			float rand = Random.Range (0f,1f);
+			if(rand>0.7f)
+				StartCoroutine("waitAndPlayShell", 1f);
+
+			Vector3 randomDir = new Vector3(shootDir.x+Random.Range (-shotSpread,shotSpread), shootDir.y+Random.Range (-shotSpread,shotSpread));
+			Transform shot = Instantiate (bullet, bulletSpawn.position, transform.rotation) as Transform;			Debug.DrawRay(bulletSpawn.position, shootDir);
+			shot.GetComponent<Rigidbody>().AddForce(randomDir+shootDir*bulletSpeed, ForceMode.Impulse);
 			yield return new WaitForSeconds (shootInterval);
 		}
+	}
+
+	IEnumerator waitAndPlayShell (float seconds){
+		yield return new WaitForSeconds(seconds);
+		bulletSpawn.GetComponent<AudioSource>().pitch = Random.Range (0.8f, 1.6f);
+		bulletSpawn.GetComponent<AudioSource> ().volume = Random.Range (0.3f, 0.6f);
+		bulletSpawn.GetComponent<AudioSource>().PlayOneShot(shellSound);
 	}
 }
