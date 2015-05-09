d659cdc4c5426669897963dba65f298e80049fd8
lordubbe <jacoblynggaardolsen@hotmail.com>
2015-05-09 18:50:24 +0200

    Improved sounds

--- Mini Game Jam/Assets/explodeOnPlayer.cs
+++ Mini Game Jam/Assets/explodeOnPlayer.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.Audio;
 public class explodeOnPlayer : MonoBehaviour {
 	public GameObject Enemy;
 	public ParticleSystem explosion;
 	public ParticleSystem explosion2;
 	private Transform player;
 	public bool dead = false;
+	public AudioClip explosionSound;
 	void Start(){
 		player = GameObject.FindWithTag ("Player").transform;
 	}
@@ -17,11 +18,14 @@ public class explodeOnPlayer : MonoBehaviour {
 			dead = true;
 			player.GetComponent<Rigidbody2D>().AddForce(Vector3.up*20f+Vector3.Normalize(player.transform.position-transform.position) * 10f, ForceMode2D.Impulse);
 
+
 		}
 
 		if (dead) {
 			Instantiate(explosion2, transform.position, Quaternion.identity);
 			Instantiate(explosion, transform.position, Quaternion.identity);
+			AudioSource.PlayClipAtPoint(explosionSound, transform.position,1f);
+
 			Destroy (Enemy);
 		}
 		     
