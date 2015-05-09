da62006fca53ecedf5a474fc8d1cbed5d8c6189f
lordubbe <jacoblynggaardolsen@hotmail.com>
2015-05-09 19:02:55 +0200

    asd

--- Mini Game Jam/Assets/waitAndKill.cs
+++ Mini Game Jam/Assets/waitAndKill.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.Audio;
 public class waitAndKill : MonoBehaviour {
-
+	public AudioClip hit;
+	public ParticleSystem bloodParticles;
+	public ParticleSystem groundHit;
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine ("waitAndDestroy", 3f);
 	}
 	
 	// Update is called once per frame
@@ -15,6 +17,24 @@ public class waitAndKill : MonoBehaviour {
 
 
 	void OnTriggerEnter2D(Collider2D col){
+		if (col.transform.tag == "Enemy") {
+			col.transform.GetComponent<EnemyAI> ().life -= 10;
+			Instantiate (bloodParticles, col.transform.position, Quaternion.identity);
+			col.GetComponent<AudioSource> ().clip = hit;
+			col.GetComponent<AudioSource> ().pitch = Random.Range (0.8f, 1.2f);
+			col.GetComponent<AudioSource> ().volume = Random.Range (0.5f, 1f);
+			col.GetComponent<AudioSource> ().PlayOneShot (hit);
+			col.transform.GetComponent<Rigidbody2D> ().AddForce ((col.transform.position - transform.position) * 10, ForceMode2D.Impulse);
+			Destroy (this.gameObject);
+		} else if (col.transform.tag == "Ground") {
+			Instantiate (groundHit, transform.position, Quaternion.identity);
+			Destroy(this.gameObject);
+		}
+	}
+
+
+	IEnumerator waitAndDestroy(float s){
+		yield return new WaitForSeconds(s);
 		Destroy (this.gameObject);
 	}
 }
